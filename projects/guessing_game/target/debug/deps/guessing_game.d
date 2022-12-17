c1f430a0d55edf88969cb9f0de9240f01ce8a25c
FrancescoXX <francescociullaroma@gmail.com>
2022-12-17 15:37:22 +0100

    guessing game lesson 3

---
+++ projects/guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\workspace\rust-crash-course\projects\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\workspace\rust-crash-course\projects\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\workspace\rust-crash-course\projects\guessing_game\target\debug\guessing_game.exe: C:\workspace\rust-crash-course\projects\guessing_game\src\main.rs
